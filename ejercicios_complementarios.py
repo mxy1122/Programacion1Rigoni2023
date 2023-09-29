b18ac82ff5381f28fede3d1cbf10b2ef82863808
mxy1122 <123978881+mxy1122@users.noreply.github.com>
2023-09-29 00:30:06 -0300

    Add files via upload

---
+++ ejercicios_complementarios.py
@@ -0,0 +1,51 @@
+#punto 1
+numero1=13 
+ #punto 2
+numero2=13.13
+#punto 3
+suma=numero1 + numero2 
+print(suma)
+#punto 4
+resta=numero1 - numero2 
+multi= numero1 * numero2 
+divi= numero1 / numero2
+print(resta)
+print(multi)
+print(divi)
+#punto 5
+nombre="maci"
+#punto 6
+precio=13.13
+#punto 7
+descuento=0.15
+#punto 8
+precio_final= 11.17
+#punto 9 
+cadena = "didi"
+#punto 10
+longitud = len(cadena)
+print(longitud)
+#punto 11
+precio2=13.13
+print(int(precio2))
+#punto 12
+nombre="didi"
+apellido="dondi"
+nombre_completo=nombre+" "+apellido
+print(nombre_completo)
+#punto 13
+edad = (20 + 5 )-10
+print(edad)
+#punto 14
+altura = (1.82 * 4 ) / 3
+print(altura)
+#punto 15 
+nombre_completo_mayuscalas = "DIDI DONDIO"
+minusculas =  nombre_completo_mayuscalas.lower()
+print(minusculas)
+#punto 16
+print("DIDI DONDIO".capitalize())
+
+
+
+
